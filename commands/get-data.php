6132c1073c17cb352c939ffc964c933cdf88a2b5
Adilet Murzaliev <adilet.murzaliev@gmail.com>
2019-02-05 22:09:35 +0600

    working script

--- commands/get-data.php
+++ commands/get-data.php
@@ -15,13 +15,21 @@ $data = [];
 
 $search = new \Google_Service_Webmasters_SearchAnalyticsQueryRequest;
 
-for ($i = 10; $i > 0; $i--){
+for ($i = 60; $i > 0; $i--) {
+    $date = date('Y-m-d', strtotime("-{$i} days"));
     $search->setStartDate(date('Y-m-d', strtotime("-{$i} days")));
     $search->setEndDate(date('Y-m-d', strtotime("-{$i} days")));
     $search->setDimensions(50);
     $search->setAggregationType('auto');
-    $impressions = $webmaster->searchanalytics->query('https://moskva.kg', $search, [])->getRows()[0]->impressions;
-    $data[] = $impressions;
+    $result = $webmaster->searchanalytics->query('https://moskva.kg', $search, [])->getRows()[0];
+    $data[] = [
+        'date'        => $date,
+        'clicks'      => $result->clicks,
+        'ctr'         => $result->ctr,
+        'impressions' => $result->impressions,
+    ];
+
+    echo "$i: ({$date}) {$result->clicks}, {$result->ctr}, {$result->impressions}\n";
 }
 
-var_export($data);
+file_put_contents(__DIR__ . '/../data.json', json_encode($data, JSON_PRETTY_PRINT));
